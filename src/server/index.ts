4b0213528e06d1dbea17efd3391269a0d04bd5af
carloscrovara <carloscrovara@gmail.com>
2023-05-18 09:32:52 -0300

    Configuracion inicial del proyecto

---
+++ src/server/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+//import { stockRouter } from "./stockRouter";
+
+//Configuracion del puerto
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+//app.use("/api/stock/", stockRouter);
+
+app.listen(PORT, () => {
+    console.log("Server running on port 3000");
+});
