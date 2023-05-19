5f2e0b0141cfbbf970b6f55bdab410e542ce6ba1
carloscrovara <carloscrovara@gmail.com>
2023-05-19 11:34:32 -0300

    Se agregaron validators con express validator

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,17 +1,33 @@
 import { Router, Request, Response, NextFunction } from "express";
 import * as controllers from "./controllers";
+import { createItemValidator } from "./validators/validators";
+import { body } from "express-validator";
 
 export const router = Router();
 
 //Ruta para crear un gasto
-router.post("/", controllers.createGastoItemController);
+router.post(
+    "/", 
+    body("importe").isNumeric().notEmpty(),
+    body("descripcion").isString().notEmpty(),
+    body("categoria").isArray().notEmpty(),
+    createItemValidator,
+    controllers.createGastoItemController
+);
 
 //Rutas para obtener todos los gastos y para obtener un gasto
 router.get("/", controllers.getGastosController );
 router.get("/:id", controllers.getGastoItemController);
 
 //Ruta para modificar un gasto
-router.put("/:id", controllers.updateGastoItemController);
+router.put(
+    "/:id", 
+    body("importe").isNumeric().notEmpty(),
+    body("descripcion").isString().notEmpty(),
+    body("categoria").isArray().notEmpty(),
+    createItemValidator,    
+    controllers.updateGastoItemController
+);
 
 //Ruta para eliminar un gasto
 router.delete("/:id", controllers.deleteGastoItemController);
