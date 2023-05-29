6852a9a8a601a2ab91a7132116c54635f9758781
carloscrovara <carloscrovara@gmail.com>
2023-05-29 11:49:18 -0300

    Configuracion inicial de Prisma

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { router } from "./router";
+import {createPrismaClient} from "./prisma"
 
 //Configuracion del puerto
 const PORT = process.env.PORT || 3000;
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use("/api/gastos/", router);
 
 app.listen(PORT, () => {
+    createPrismaClient()
     console.log("Server running on port 3000");
 });
