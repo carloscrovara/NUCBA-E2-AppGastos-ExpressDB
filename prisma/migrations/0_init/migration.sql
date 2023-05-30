0a7dc953e5478e762e02102c1e32e8f2c467a72c
carloscrovara <carloscrovara@gmail.com>
2023-05-30 20:15:35 -0300

    Avance con Prisma

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,21 @@
+Need to install the following packages:
+  prisma@4.15.0
+Ok to proceed? (y) -- CreateTable
+CREATE TABLE `categorias` (
+    `id` VARCHAR(36) NOT NULL DEFAULT (uuid()),
+    `nombre` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `gastos` (
+    `id` VARCHAR(36) NOT NULL DEFAULT (uuid()),
+    `fechaCreacion` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `importe` FLOAT NOT NULL,
+    `descripcion` VARCHAR(255) NOT NULL,
+    `categoriaId` VARCHAR(36) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
